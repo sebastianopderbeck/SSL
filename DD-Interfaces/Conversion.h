2722d36ef745a1ed8189b3e5ba90454cab90e449
sebastianopderbeck <sopderbeck@frba.utn.edu.ar>
2023-06-06 22:06:26 -0300

    adding conversion.h

---
+++ DD-Interfaces/Conversion.h
@@ -0,0 +1,9 @@
+#ifndef __CONVERSION_H__
+#define __CONVERSION_H__
+
+void askValues(int* lower, int* upper, int* step);
+void headerTable(char* degreeToConvert, char* conversionDegree);
+float conversionFormula(float degree, int isFahrenheitToCelsius);
+void conversionTable(int lower, int upper, int step, int isFahrenheitToCelsius);
+
+#endif;
